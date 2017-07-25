23eb679393a9323116d8f1d318f15b6e94b7d399
abhirammv <amadenur@ucsc.edu>
2017-07-24 18:40:04 -0700

    Day 5 - Loops

---
+++ HackerRank/30Days/Day5_Loops.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Abhiram MV on 7/24/17.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1; i <= 10; i++)
+        cout<<n<<" "<<"x "<<i<<" "<<"= "<<(n*i)<<endl;
+    return 0;
+
+}
