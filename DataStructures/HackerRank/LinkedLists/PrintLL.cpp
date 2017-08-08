2bc7130a1f8a6c265160959d4155b9ae0da51c1d
abhirammv <amadenur@ucsc.edu>
2017-08-07 19:19:31 -0700

    edit file 1

--- DataStructures/HackerRank/LinkedLists/PrintLL.cpp
+++ DataStructures/HackerRank/LinkedLists/PrintLL.cpp
@@ -1,12 +1,25 @@
-//
-// Created by Abhiram MV on 8/7/17.
-//
+/*
+  Print elements of a linked list on console
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
 #include <iostream>
-
 using namespace std;
 
-int main()
+void Print(Node *head)
 {
-
-    return 0;
+    // This is a "method-only" submission.
+    // You only need to complete this method.
+    Node* start = head;
+    while(start!=NULL)
+    {
+        cout<<start->data<<endl;
+        start = start->next;
+    }
 }
+
