eda064ff44a76987afb819f6a95deb6bc1e498aa
abhirammv <amadenur@ucsc.edu>
2017-08-07 19:45:14 -0700

    edit file 2

---
+++ DataStructures/HackerRank/LinkedLists/InsertElementTail.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Abhiram MV on 8/7/17.
+//
+
+#include <iostream>
+using namespace std;
+
+/*
+Insert Node at the end of a linked list
+        head pointer input could be NULL as well for empty list
+Node is defined as
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+*/
+
+Node* Insert(Node *head,int data) {
+    Node* node = new Node;
+    node->data = data;
+
+    if(head==NULL)
+        return node;
+
+    else
+    {
+        Node* start = head;
+        while(start->next!=NULL)
+            start = start->next;
+
+        start->next = node;
+        return head;
+
+    }
+
+}
